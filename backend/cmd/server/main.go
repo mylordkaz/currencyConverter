64e87804d708707eca71c814ed07308011db73a6
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-24 00:29:59 +0900

    feat: load config to server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+
+	"github.com/mylordkaz/currencyConverter/backend/config"
+)
+
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatalf("failed to load config: %v", err)
+	}
+}

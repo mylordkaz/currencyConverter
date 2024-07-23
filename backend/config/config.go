7cc2972218f06050e04a76ee35f195d76f7a9c91
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-24 00:26:33 +0900

    feat: Load func for the API

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,6 +1,27 @@
 package config
 
+import (
+	"encoding/json"
+	"os"
+)
+
 type Config struct {
 	FiatAPIURL 		string `json:"fiatApiUrl"`
 	CryptoAPIURL	string `json:"cryptoApiUrl"`
+}
+
+func Load() (*Config, error) {
+	file, err := os.Open("config.json")
+	if err != nil {
+		return nil, err 
+	}
+	defer file.Close()
+
+	var cfg Config
+	err = json.NewDecoder(file).Decode(&cfg)
+	if err != nil {
+		return nil, err 
+	}
+
+	return &cfg, nil
 }

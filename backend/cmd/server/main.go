02a7cc4877c701fe0893cd54603ee4447b8da3bd
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-24 19:43:26 +0900

    Load the env file

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/mylordkaz/currencyConverter/backend/config"
+	
 )
 
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("failed to load config: %v", err)

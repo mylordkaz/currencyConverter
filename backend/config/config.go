cc9442f5a25309df1ef25af7bf08ec1a1e575f14
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-25 22:00:35 +0900

    update config to get apikeys and port from env

--- backend/config/config.go
+++ backend/config/config.go
@@ -7,21 +7,36 @@ import (
 
 type Config struct {
 	FiatAPIURL 		string
+	FiatAPIKEY		string
 	CryptoAPIURL	string
+	CryptoAPIKEY	string
+	Port			string
 }
 
 func Load() (*Config, error) {
 	fiatAPIURL := os.Getenv("FIAT_API_URL")
-	if fiatAPIURL == "" {
-		return nil, fmt.Errorf("FIAT_API_URL env is not set")
+	fiatAPIKEY := os.Getenv("FIAT_API_KEY")
+	if fiatAPIKEY == "" {
+		return nil, fmt.Errorf("FIAT_API_KEY env is not set")
 	}
 	cryptoAPIURL := os.Getenv("CRYPTO_API_URL")
-	if cryptoAPIURL == ""{
-		return nil, fmt.Errorf("CRYPTO_API_URL env is not set")
+	cryptoAPIKEY := os.Getenv("CRYPTO_API_KEY")
+	if cryptoAPIKEY == ""{
+		return nil, fmt.Errorf("CRYPTO_API_KEY env is not set")
 	}
 
 	return &Config{
 		FiatAPIURL: fiatAPIURL,
+		FiatAPIKEY: fiatAPIKEY,
 		CryptoAPIURL: cryptoAPIURL,
+		Port: getEnvDefault("PORT", "8080"),
 	}, nil
+}
+
+func getEnvDefault(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
 }

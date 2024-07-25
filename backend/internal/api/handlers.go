22f1fa83110da8b05722138a79641b9ef9107dd1
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-25 23:26:47 +0900

    feat; create the handler to get currencies

--- backend/internal/api/handlers.go
+++ backend/internal/api/handlers.go
@@ -1,15 +1,26 @@
 package api
 
 import (
-	
+	"net/http"
 
+	"github.com/gin-gonic/gin"
 	"github.com/mylordkaz/currencyConverter/backend/internal/service"
 )
 
 
 type Handler struct {
-	converter *service.Converter
+	currencyService *service.CurrencyService
 }
 
+func NewHandler(currencyService *service.CurrencyService) *Handler {
+	return &Handler{currencyService: currencyService}
+}
 
-
+func (h *Handler) GetCurrencies(c *gin.Context) {
+	currencies, err := h.currencyService.FetchCurrencies()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, currencies)
+}

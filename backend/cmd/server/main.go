1f3c2f6bb1342384c1e73894bf98a72453d29d10
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-25 23:33:02 +0900

    import config, handle fetch, start server

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"log"
+	"net/http"
 
-	"github.com/joho/godotenv"
+	"github.com/gin-gonic/gin"
 	"github.com/mylordkaz/currencyConverter/backend/config"
-	
+	"github.com/mylordkaz/currencyConverter/backend/internal/api"
+	"github.com/mylordkaz/currencyConverter/backend/internal/service"
 )
 
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
 
 	cfg, err := config.Load()
 	if err != nil {
 		log.Fatalf("failed to load config: %v", err)
+	}
+
+	currencyService := service.NewCurrencyService(cfg.FiatAPIURL, cfg.FiatAPIKEY)
+	handler := api.NewHandler(currencyService)
+
+	r := gin.Default()
+	r.GET("/api/currencies", handler.GetCurrencies)
+
+	log.Printf("Starting server on %s", cfg.Port)
+	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
+		log.Fatalf("Failed to start server %s", err)
 	}
 }

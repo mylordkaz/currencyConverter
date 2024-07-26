f5375ac1ce9fcea4556595545c9d3da7de3a25b8
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-26 23:26:59 +0900

    remove unUsed struct

--- backend/internal/models/currency.go
+++ backend/internal/models/currency.go
@@ -1,17 +1,5 @@
 package models
 
-type CurrencyType string
-
-const (
-	Fiat CurrencyType = "fiat"
-	Crypto CurrencyType = "crypto"
-)
-
-type Currency struct {
-	Code string
-	Name string
-	Type CurrencyType
-}
 
 type ExchangeRates struct {
 	Base 	string 				`json:"base"`

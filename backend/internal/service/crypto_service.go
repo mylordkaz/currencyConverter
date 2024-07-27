56cb069c74b1308ad1b725d6c598426db5e50322
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-28 00:54:29 +0900

    create the fetch crypto func

--- backend/internal/service/crypto_service.go
+++ backend/internal/service/crypto_service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/mylordkaz/currencyConverter/backend/internal/models"
 	"github.com/mylordkaz/currencyConverter/backend/pkg/utils"
 )
 
@@ -21,3 +24,30 @@ func NewCryptoService(apiURL, apiKey string) *CryptoService {
 	}
 }
 
+func (s *CryptoService) FetchCrypto() (*models.CryptoRates, error) {
+	url := fmt.Sprintf("%s/v1/cryptocurrency/listing/latest", s.apiURL)
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, fmt.Errorf("error creating the request %w", err)
+	}
+
+	req.Header.Set("X-CMC_PRO_API_KEY", s.apiKey)
+
+	resp, err :=s.client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error making the request %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("API request failed with status code: %d", resp.StatusCode)
+	}
+
+	var cryptoResponse models.CryptoResponse
+	if err := json.NewDecoder(resp.Body).Decode(&cryptoResponse); err != nil {
+		return nil, fmt.Errorf("error decoding response: %w", err)
+	}
+
+	return &cryptoResponse.Data, nil
+}

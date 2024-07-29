a5a6b1f8be471c41e1901ed6ea7fe740ac2b3de0
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-07-30 00:10:35 +0900

    inital commit frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <div className="text-center">
+        <h1 className="text-3xl font-bold underline">Tsukakan</h1>
+      </div>
+    </>
+  );
+}

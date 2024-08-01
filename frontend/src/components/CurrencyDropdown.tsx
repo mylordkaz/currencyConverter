e79b12bc833fe45dc80c5a4c70df1fb3ae96a8c3
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-08-02 00:09:06 +0900

    add a svg dropdown sign

--- frontend/src/components/CurrencyDropdown.tsx
+++ frontend/src/components/CurrencyDropdown.tsx
@@ -52,6 +52,15 @@ const CurrencyDropdown: React.FC<CurrencyDropdownProps> = ({
       >
         <span>{currencies.find((c) => c.code === selectedCurrency)?.flag}</span>
         <span> {selectedCurrency}</span>
+        <div className="pointer-events-none flex items-center px-2 text-white">
+          <svg
+            className="fill-current h-4 w-4"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+          </svg>
+        </div>
       </div>
       {isOpen && (
         <div className="absolute mt-1 ml-4 w-auto bg-gray-200 rounded-lg shadow-lg z-10">

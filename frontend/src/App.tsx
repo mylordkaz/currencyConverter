057635397c666738a7ea41576b50495bf17b90c0
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-08-07 00:09:11 +0900

    add drag and drop function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,12 @@ import React, { useEffect, useState } from 'react';
 import CurrencyDropdown from './components/CurrencyDropdown';
 import { currencyCodes, currencyInfo } from './service/currencyInfo';
 import CurrencySelectionModal from './components/CurrencySelectionModal';
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from 'react-beautiful-dnd';
 
 interface Currency {
   code: string;
@@ -71,6 +77,7 @@ export default function App() {
   const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setAmount(e.target.value);
   };
+
   const handleCurrenciesList = (currencyCode: string) => {
     setCurrencyList((prev) =>
       prev.includes(currencyCode)
@@ -79,75 +86,120 @@ export default function App() {
     );
   };
 
+  const handleOnDragEnd = (result: DropResult) => {
+    if (!result.destination) {
+      return;
+    }
+
+    const items = Array.from(currencyList);
+    const [reorderItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderItem);
+
+    setCurrencyList(items);
+  };
+
   return (
-    <div className="min-h-screen bg-gradient-to-b from-blue-400 to-blue-900 flex items-center justify-center p-4">
-      <div className="w-full max-w-md">
-        <h1 className="text-4xl font-bold mb-8 text-center text-black">
-          Currency Converter
-        </h1>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <div className="min-h-screen bg-gradient-to-b from-blue-400 to-blue-900 flex items-center justify-center p-4">
+        <div className="w-full max-w-md">
+          <h1 className="text-4xl font-bold mb-8 text-center text-black">
+            Currency Converter
+          </h1>
 
-        <div className="space-y-4">
-          <CurrencyDropdown
-            currencies={currencies}
-            selectedCurrency={baseCurrency}
-            onSelect={handleBaseCurrencyChange}
-          />
+          <div className="space-y-4">
+            <CurrencyDropdown
+              currencies={currencies}
+              selectedCurrency={baseCurrency}
+              onSelect={handleBaseCurrencyChange}
+            />
 
-          <div className="bg-slate-400 rounded-3xl p-4 flex items-center justify-between">
-            <div className="flex items-center flex-grow">
-              <span className="text-white text-3xl font-bold mr-2">
-                {currencies.find((c) => c.code === baseCurrency)?.symbol}
-              </span>
-              <input
-                type=""
-                value={amount}
-                onChange={handleAmountChange}
-                className="bg-transparent text-white text-3xl font-bold focus:outline-none w-full"
+            <div className="bg-slate-400 rounded-3xl p-4 flex items-center justify-between">
+              <div className="flex items-center flex-grow">
+                <span className="text-white text-3xl font-bold mr-2">
+                  {currencies.find((c) => c.code === baseCurrency)?.symbol}
+                </span>
+                <input
+                  type=""
+                  value={amount}
+                  onChange={handleAmountChange}
+                  className="bg-transparent text-white text-3xl font-bold focus:outline-none w-full"
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full max-w-md bg-white rounded-3xl shadow-lg mt-6 p-6">
+            <h1 className="text-2xl font-bold mb-4">Fiat</h1>
+
+            <Droppable droppableId="currency">
+              {(provided) => (
+                <div
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                  className="space-y-2"
+                >
+                  {currencies
+                    .filter((currency) => currencyList.includes(currency.code))
+                    .map((currency, index) => (
+                      <Draggable
+                        key={currency.code}
+                        draggableId={currency.code}
+                        index={index}
+                      >
+                        {(provided) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            className="flex items-center justify-between bg-gray-50 rounded-xl p-3"
+                          >
+                            <div className="flex items-center">
+                              <span className="text-2xl mr-2">
+                                {currency.flag}
+                              </span>
+                              <span className="font-semibold">
+                                {currency.code}
+                              </span>
+                            </div>
+                            <div className="flex items-center">
+                              <div className="text-right">
+                                <div className="font-semibold">
+                                  {amount
+                                    ? (
+                                        currency.rate * parseFloat(amount)
+                                      ).toFixed(2)
+                                    : '0.00'}
+                                </div>
+                                <div className="text-xs text-gray-500">
+                                  {currency.description}
+                                </div>
+                              </div>
+                              <button className="ml-4 text-gray-400">⋮</button>
+                            </div>
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                  {currencyList.length === 0 && (
+                    <div>
+                      No currencies selected. Please add some currencies.
+                    </div>
+                  )}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+
+            <div className="mt-4 flex justify-end">
+              <CurrencySelectionModal
+                currencies={currencies}
+                selectedCurrency={currencyList}
+                onCurrencySelected={handleCurrenciesList}
               />
             </div>
           </div>
         </div>
-
-        <div className="w-full max-w-md bg-white rounded-3xl shadow-lg mt-6 p-6">
-          <h1 className="text-2xl font-bold mb-4">Fiat</h1>
-
-          <div className="space-y-2">
-            {currencies
-              .filter((currency) => currencyList.includes(currency.code))
-              .map((currency) => (
-                <div
-                  key={currency.code}
-                  className="flex items-center justify-between bg-gray-50 rounded-xl p-3"
-                >
-                  <div className="flex items-center">
-                    <span className="text-2xl mr-2">{currency.flag}</span>
-                    <span className="font-semibold">{currency.code}</span>
-                  </div>
-                  <div className="flex items-center">
-                    <div className="text-right">
-                      <div className="font-semibold">
-                        {amount
-                          ? (currency.rate * parseFloat(amount)).toFixed(2)
-                          : '0.00'}
-                      </div>
-                      <div className="text-xs text-gray-500">
-                        {currency.description}
-                      </div>
-                    </div>
-                    <button className="ml-4 text-gray-400">⋮</button>
-                  </div>
-                </div>
-              ))}
-          </div>
-          <div className="mt-4 flex justify-end">
-            <CurrencySelectionModal
-              currencies={currencies}
-              selectedCurrency={currencyList}
-              onCurrencySelected={handleCurrenciesList}
-            />
-          </div>
-        </div>
       </div>
-    </div>
+    </DragDropContext>
   );
 }

57a4f789d9a25f98b05f96d18c7dae28326e1202
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-08-26 22:29:42 +0900

    add the safe view area

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import tw from 'twrnc';
 import CurrencySelector from '@/components/CurrencySelector';
@@ -10,13 +10,15 @@ export default function Index() {
       colors={['#60A5FA', '#2563EB']} // Light blue to darker blue
       style={tw`flex-1`}
     >
-      <View style={tw`flex-1 items-center p-4`}>
-        <Text style={tw`text-4xl font-bold mb-8 text-center text-black`}>
-          Tsukakan
-        </Text>
-        <CurrencySelector />
-        <CurrencyList />
-      </View>
+      <SafeAreaView style={tw`flex-1`}>
+        <View style={tw`flex-1 p-4`}>
+          <Text style={tw`text-4xl font-bold mb-8 text-center text-black`}>
+            Tsukakan
+          </Text>
+          <CurrencySelector />
+          <CurrencyList />
+        </View>
+      </SafeAreaView>
     </LinearGradient>
   );
 }

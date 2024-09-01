90cc93199ad4dc599fc1d0422462c99c9b763987
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-09-01 18:02:03 +0900

    add logic to save list locally

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,10 +1,11 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SafeAreaView, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import tw from 'twrnc';
 import CurrencySelector from '@/components/CurrencySelector';
 import CurrencyList from '@/components/CurrencyList';
 import useCurrencies from '@/hooks/useCurrencies';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Currency } from '@/constants/type';
 
 export default function Index() {
@@ -18,6 +19,36 @@ export default function Index() {
 
   const allCurrencies = [...fiatCurrencies, ...cryptoCurrencies];
 
+  useEffect(() => {
+    loadSelectedCurrencies();
+  }, []);
+
+  useEffect(() => {
+    saveSelectedCurrencies();
+  }, [selectedCurrencies]);
+
+  const loadSelectedCurrencies = async () => {
+    try {
+      const savedCurrencies = await AsyncStorage.getItem('selectedCurrencies');
+      if (savedCurrencies) {
+        setSelectedCurrencies(JSON.parse(savedCurrencies));
+      }
+    } catch (error) {
+      console.error('Error loading currencies', error);
+    }
+  };
+
+  const saveSelectedCurrencies = async () => {
+    try {
+      await AsyncStorage.setItem(
+        'selectedCurrencies',
+        JSON.stringify(selectedCurrencies)
+      );
+    } catch (error) {
+      console.error('Error saving currencies', error);
+    }
+  };
+
   const handleCurrencyChange = (currency: string) => {
     setSelectedCurrency(currency);
   };
@@ -29,6 +60,11 @@ export default function Index() {
     if (!selectedCurrencies.includes(currency.code)) {
       setSelectedCurrencies([...selectedCurrencies, currency.code]);
     }
+  };
+  const handleRemoveCurrency = (currencyCode: string) => {
+    setSelectedCurrencies(
+      selectedCurrencies.filter((code) => code !== currencyCode)
+    );
   };
 
   return (
@@ -50,11 +86,13 @@ export default function Index() {
           />
           <CurrencyList
             currencies={allCurrencies}
+            selectedCurrencyCodes={selectedCurrencies}
             baseCurrency={selectedCurrency}
             baseAmount={parseFloat(amount) || 0}
             isLoading={isLoading}
             error={error}
             onAddCurrency={handleAddCurrency}
+            onRemoveCurrency={handleRemoveCurrency}
             availableCurrencies={allCurrencies}
           />
         </View>

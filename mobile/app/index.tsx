b24f8d5493474a1d274e5bb877b43756cff099a1
Kevin <128312170+mylordkaz@users.noreply.github.com>
2024-09-01 17:08:24 +0900

    wrap into a Suspense

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -4,7 +4,7 @@ import tw from 'twrnc';
 import CurrencySelector from '@/components/CurrencySelector';
 import CurrencyList from '@/components/CurrencyList';
 import useCurrencies from '@/hooks/useCurrencies';
-import { Suspense, useState } from 'react';
+import { useState } from 'react';
 import { Currency } from '@/constants/type';
 
 export default function Index() {
@@ -32,35 +32,33 @@ export default function Index() {
   };
 
   return (
-    <Suspense fallback={<Text>Loading</Text>}>
-      <LinearGradient
-        colors={['#60A5FA', '#2563EB']} // Light blue to darker blue
-        style={tw`flex-1`}
-      >
-        <SafeAreaView style={tw`flex-1`}>
-          <View style={tw`flex-1 p-4`}>
-            <Text style={tw`text-4xl font-bold mb-8 text-center text-black`}>
-              Tsukakan
-            </Text>
-            <CurrencySelector
-              currencies={allCurrencies}
-              selectedCurrency={selectedCurrency}
-              onCurrencyChange={handleCurrencyChange}
-              amount={amount}
-              onAmountChange={handleAmountChange}
-            />
-            <CurrencyList
-              currencies={allCurrencies}
-              baseCurrency={selectedCurrency}
-              baseAmount={parseFloat(amount) || 0}
-              isLoading={isLoading}
-              error={error}
-              onAddCurrency={handleAddCurrency}
-              availableCurrencies={allCurrencies}
-            />
-          </View>
-        </SafeAreaView>
-      </LinearGradient>
-    </Suspense>
+    <LinearGradient
+      colors={['#60A5FA', '#2563EB']} // Light blue to darker blue
+      style={tw`flex-1`}
+    >
+      <SafeAreaView style={tw`flex-1`}>
+        <View style={tw`flex-1 p-4`}>
+          <Text style={tw`text-4xl font-bold mb-8 text-center text-black`}>
+            Tsukakan
+          </Text>
+          <CurrencySelector
+            currencies={allCurrencies}
+            selectedCurrency={selectedCurrency}
+            onCurrencyChange={handleCurrencyChange}
+            amount={amount}
+            onAmountChange={handleAmountChange}
+          />
+          <CurrencyList
+            currencies={allCurrencies}
+            baseCurrency={selectedCurrency}
+            baseAmount={parseFloat(amount) || 0}
+            isLoading={isLoading}
+            error={error}
+            onAddCurrency={handleAddCurrency}
+            availableCurrencies={allCurrencies}
+          />
+        </View>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
